8980badcd2983d32b374f1a96143b1b822bd51fd
Galahorn <88438163+Galahorn@users.noreply.github.com>
2021-10-13 18:30:15 +0900

    임플리먼트 작업

--- javateam/src/javateam/MulGame.java
+++ javateam/src/javateam/MulGame.java
@@ -1,5 +1,23 @@
 package javateam;
 
-public class MulGame {
+public class MulGame implements iGame{
+
+	@Override
+	public void makeRandom() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getQuizMsg() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean checkAnswer(int answer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
--- javateam/src/javateam/iGame.java
+++ javateam/src/javateam/iGame.java
@@ -2,4 +2,9 @@ package javateam;
 
 public interface iGame {
 
+	public void makeRandom();
+
+	public String getQuizMsg();
+
+	public boolean checkAnswer(int answer);
 }
