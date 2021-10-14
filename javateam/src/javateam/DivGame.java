5835864a40a7ad56cd192def47249fc6eccf3412
Galahorn <kpgyp18@naver.com>
2021-10-14 14:05:08 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of
    https://github.com/2021-SMHRD-KDT-Bigdata-7/CalculatorGame3.git

--- javateam/src/javateam/DivGame.java
+++ javateam/src/javateam/DivGame.java
@@ -1,5 +1,23 @@
 package javateam;
 
-public class DivGame {
+public class DivGame implements iGame {
+
+	@Override
+	public void makeRandom() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getQuizMsg() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean checkAnswer(int answer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
--- javateam/src/javateam/iGame.java
+++ javateam/src/javateam/iGame.java
@@ -2,4 +2,10 @@ package javateam;
 
 public interface iGame {
 
+	public void makeRandom();
+
+	public String getQuizMsg();
+
+	public boolean checkAnswer(int answer);
+
 }
